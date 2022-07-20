0d10b70c78133ae072a107a57201a47def4b4cee
droboshok <droboshok@yahoo.com>
2022-07-20 21:09:36 +0300

    vse

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ var kit = require("gulp-kit-2");
 var gcmq = require("gulp-group-css-media-queries");
 var autoprefixer = require("gulp-autoprefixer");
 var del = require("del");
+var svgSprite = require("gulp-svg-sprite");
+  var svgmin = require("gulp-svgmin");
+  var cheerio = require("gulp-cheerio");
+  var replace = require("gulp-replace");
 
 // sass.compiler = require("node-sass");
 gulp.task("clean", function() {
@@ -48,6 +52,48 @@ gulp.task("kit", async function() {
 //     .pipe(gulp.dest("dest/"))
 //     .pipe(browserSync.stream());
 // });
+gulp.task("svg", function () {
+  return (
+    gulp
+      .src("./src/svg/*.svg")
+      // minify svg
+      .pipe(
+        svgmin({
+          js2svg: {
+            pretty: true,
+          },
+        })
+      )
+
+      // remove all fill, style and stroke declarations in out shapes
+      .pipe(
+        cheerio({
+          run: function ($) {
+            $("[fill]").removeAttr("fill");
+            $("[stroke]").removeAttr("stroke");
+            $("[style]").removeAttr("style");
+          },
+          parserOptions: { xmlMode: true },
+        })
+      )
+      // cheerio plugin create unnecessary string '&gt;', so replace it.
+      .pipe(replace("&gt;", ">"))
+      // build svg sprite
+      .pipe(
+        svgSprite({
+          mode: {
+            symbol: {
+              sprite: "../sprite.svg",
+            },
+          },
+        })
+      )
+
+      .pipe(gulp.dest("dest/img/svg/"))
+  );
+});
+
+
 
 
 gulp.task("serve", function() {
@@ -59,6 +105,7 @@ gulp.task("serve", function() {
   gulp.watch("src/styles/**/*.scss", gulp.series("sass"));
   // gulp.watch("src/kit/**/*.html", gulp.series("fileinclude"));
   gulp.watch("./src/**/*.kit", gulp.series("kit"));
+  gulp.watch("./src/svg/*.svg", gulp.series("svg"));
   gulp.watch("dest/*.html").on("change", browserSync.reload);
 });
 
